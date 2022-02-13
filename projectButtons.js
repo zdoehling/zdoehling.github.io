1a2dc1a78c5c4a10addd7ff23b00a9f5fa81e67e
zdoehling <43963520+zdoehling@users.noreply.github.com>
2022-02-14 08:31:15 +0900

    2nd batch

---
+++ projectButtons.js
@@ -0,0 +1,42 @@
+var tileLinks = [
+    "gatech.html",
+    "ati.html",
+    "heritage.html",
+    "winshape.html",
+    "venmo.html",
+    "soarSouth.html",
+    "stride.html",
+    "thesis.html",
+    "inPrize.html",
+    "website.html"
+];
+
+var previous = "'location.href= " + tileLinks[tileLinks.length - 1] + "'";
+var next = "heritage.html";
+
+var bottomButtons = document.getElementsByClassName("dirButton");
+
+var currentPage = document.location.href.substring(document.location.href.lastIndexOf( '/' )+1);
+
+function prevClick()
+{
+    if(tileLinks.indexOf(currentPage, fromIndex = 0) == 0){
+        window.location.href = tileLinks[tileLinks.length - 1];
+    } else{
+        window.location.href = tileLinks[tileLinks.indexOf(currentPage, fromIndex = 0) - 1];
+    }
+}
+function nextClick()
+{
+    if(tileLinks.indexOf(currentPage, fromIndex = 0) == (tileLinks.length - 1)){
+        window.location.href = tileLinks[0];
+    } else{
+        window.location.href = tileLinks[tileLinks.indexOf(currentPage, fromIndex = 0) + 1];
+    }
+}
+
+bottomButtons[0].onclick=prevClick;
+bottomButtons[1].onclick=nextClick;
+
+
+
